dd1a0ca16db8eefc9ea01e877e13d95d206769d0
iArshad157 <iarshadalam2003@gmail.com>
2025-07-05 22:15:57 +0530

    Add Project Files

---
+++ public/css/js/map.js
@@ -0,0 +1,13 @@
+//  <script>
+// 	mapboxgl.accessToken = mapToken;
+//     const map = new mapboxgl.Map({
+//         container: 'map', // container ID
+//         style:"mapbox://styles/mapbox/streets-v12",
+//         center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+//         zoom: 9 // starting zoom
+//     });
+// </script>
+
+// const marker1 = new mapboxgl.Marker()
+//         .setLngLat(coordinates)//listing.geometry.coordintes
+//         .addTo(map);
